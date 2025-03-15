a70f70cb02e63f452007b060096c5294b1e242b6
s3tra <dropemail01@pm.me>
2025-03-15 13:00:09 +0000

    Seperate code into functions

--- src/events/interactionCreate.js
+++ src/events/interactionCreate.js
@@ -1,6 +1,32 @@
 import { MessageFlags, PermissionsBitField } from 'discord.js';
 import { getData } from 'storelite2';
 
+const checkPermissions = async (interaction, guildData) => {
+  try {
+    let modUsers = guildData.modUsers;
+
+    if (
+      !modUsers.includes(interaction.user.id) &&
+      !interaction.member.permissions.has(
+        PermissionsBitField.Flags.Administrator
+      )
+    )
+      return false;
+
+    return true;
+  } catch (error) {
+    await interaction.reply({
+      content: 'An error occured while checking permissions.',
+      flags: MessageFlags.Ephemeral,
+    });
+
+    return console.error(
+      `An error occured while checking permissions: `,
+      error
+    );
+  }
+};
+
 const execute = async (interaction) => {
   if (interaction.isChatInputCommand()) {
     const command = interaction.client.commands.get(interaction.commandName);
@@ -15,14 +41,7 @@ const execute = async (interaction) => {
     if (!guildData) guildData = { _key: interaction.guild.id, modUsers: [] };
     if (!guildData.modUsers) guildData.modUsers = [];
 
-    let modUsers = guildData.modUsers;
-
-    if (
-      !modUsers.includes(interaction.user.id) &&
-      !interaction.member.permissions.has(
-        PermissionsBitField.Flags.Administrator
-      )
-    )
+    if (!checkPermissions(interaction, guildData))
       return interaction.reply({
         content: 'You do not have permission to run this command.',
         flags: MessageFlags.Ephemeral,
